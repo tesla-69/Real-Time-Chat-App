5cdc4c7632171f95cfb252b4a4abcfea2cc0be18
KartikShah369 <shahkartik369@gmail.com>
2025-06-19 23:20:40 +0530

    fixed error in index.js of backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,12 +29,12 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV==="production") {
-  app.use(express.static(path.json(__dirname, "../frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.json(__dirname, "../frontend", "dist", "index.html"))
-  })
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 server.listen(PORT, () => {
