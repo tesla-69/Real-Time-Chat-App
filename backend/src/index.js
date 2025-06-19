a4add7009e170dc952fed289d189e61049874768
KartikShah369 <shahkartik369@gmail.com>
2025-06-19 21:48:22 +0530

    feat: add chat-app package and update dependencies in frontend and backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,11 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 
+import path from "path"
 
 dotenv.config();
-
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 
 app.use(express.json({ limit: '10mb' })); // or even 20mb if needed
@@ -26,7 +28,14 @@ app.use(
 );
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-const PORT = process.env.PORT || 5001;
+
+if(process.env.NODE_ENV==="production") {
+  app.use(express.static(path.json(__dirname, "../frontend/dist")))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.json(__dirname, "../frontend", "dist", "index.html"))
+  })
+}
 
 server.listen(PORT, () => {
   console.log("server is running on port:", PORT);
