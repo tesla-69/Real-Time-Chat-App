f3d245b837275f9c364ff836be9896c22e0ad37f
KartikShah369 <shahkartik369@gmail.com>
2025-06-20 00:10:21 +0530

    increased limit for file upload

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,9 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
 app.use(cookieParser());
 app.use(
   cors({
